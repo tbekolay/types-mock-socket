149f9bffffd15b5ef81be279394611938c0a7bdc
Trevor Bekolay <tbekolay@gmail.com>
2016-10-12 09:14:45 -0400

    Initial commit with minimal working definitions

---
+++ README.md
@@ -0,0 +1,8 @@
+# Typed Mock Socket [![Build Status](https://travis-ci.org/tbekolay/types-mock-socket.svg?branch=master)](https://travis-ci.org/tbekolay/types-mock-socket)
+
+Type definitions for
+[`mock-socket`](https://github.com/thoov/mock-socket).
+
+## LICENSE
+
+MIT
---
+++ src/mock-socket.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for mock-socket 5.0.1
+// Project: https://github.com/thoov/mock-socket
+// Definitions by: Trevor Bekolay <https://github.com/tbekolay>
+// Definitions: https://github.com/tbekolay/types-
+
+/// <reference path="../typings/globals/socket.io/index.d.ts"/>
+
+export var WebSocket: WebSocket;
+export var Server: SocketIO.Server;
+export var SocketIO: SocketIOStatic;
